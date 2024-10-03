62e6e0a821ab48902f3e058dc836f0efa9fe9932
alejandro gomez <alejandro.gomez.rivera.al@iespoligonosur.org>
2024-10-03 20:05:58 +0200

    39

--- Protectora/models.py
+++ Protectora/models.py
@@ -6,12 +6,12 @@ from django.utils import timezone
 # Create your models here.
 
 class Animales(models.Model):
-    Cuidador = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="Protectora")
+    Cuidador = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     nombre = models.CharField(max_length = 200)
     tipo = models.CharField(max_length = 200)
 
     def __str__(self):
-        return self.title
+        return self.nombre
 
 
 class protectora(models.Model):
@@ -20,11 +20,12 @@ class protectora(models.Model):
     Fecha_creacion = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.title
+        return self.nombre
 
 class Colabroador(models.Model):
     nombre = models.CharField(max_length = 200)
     cargo = models.CharField(max_length = 200)
     Fecha_entrada_protectora = models.DateTimeField(default=timezone.now)
 
-
+    def __str__(self):
+        return self.nombre
--- Protectora/views.py
+++ Protectora/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import protectora
 
 # Create your views here.
+#def post_list(request) :
+#    protectora = protectora.objects.all()
+#    return render(request, 'Protectora/post_list.html', {'posts_mostrar': protectora})
+
+
+def post_list(request) :
+    Protectora = protectora.objects.all()
+    return render(request, 'protectora/post_list.html', {'protectora_mostrar': Protectora})
